b9dc64039cfbf0c51ed6ca5f8384dbb08faefb8d
waleed2k03 <waleedgamer2k20@gmail.com>
2024-05-17 19:16:32 +0400

    waleed

---
+++ frontend/codesmith/src/app/waleed.js
@@ -0,0 +1,139 @@
+// import './Header.css';
+
+
+import './dady.css';
+import React from 'react';
+import picx from "./hash.jpg" ;
+import ash from "./ash.jpg";
+import shas from "./shas.jpg";
+import asthetic from "./asthetic.jpg";
+
+import Image from "next/image"
+
+function App() {
+  return (
+    <div className="container">
+      <header className="header">
+        <div className="logo">CS</div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <a href="#">Landing page</a>
+            </li>
+            <li>
+              <a href="#">Data Entry Page</a>
+            </li>
+            <li>
+              <a href="#">Reports page</a>
+            </li>
+            <li>
+              <a href="#">Admin Panel</a>
+            </li>
+           <div className="blues">
+           <button class ="auth"> <a href="#">SIGN UP</a>
+           </button>
+           <button class="auth"> <a href="#">LOG IN</a>
+            </button>
+            </div>
+           </ul>
+          
+        </nav>
+        
+  
+      </header>
+      <div class="line"></div>
+      <div className="img">
+          <Image src={picx} alt="Your image description" className="bottom-image" />
+          <div class="img-text">Welcome to codesmith</div>
+      </div>
+      <main className="main">
+        <section className="hero">
+          <ul>
+          <h2>WELCOME TO CODESMITH!</h2>
+          </ul>
+          <div className="amg">
+          <Image src={ash} alt="Your image description" className="amg" />
+      </div>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          </p>
+
+<div className="shas">
+          <Image src={shas} alt="Your image description" className="shas" />
+      </div>
+      <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          </p>
+
+          <div class="linetwo"></div>
+
+          <div className="asthetic">
+          <Image src={asthetic} alt="Your image description" className="asthetic" />
+      </div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          </p>
+          </section>
+        
+        {/* ... Other sections in your main content */}
+        <section className="head">
+        <div class="linethree"></div>
+      <h4>Header text</h4>
+      
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus eget laoreet rutrum. Pellentesque at lorem vel mi placerat vehicula eget nec urna. In hac habitasse platea dictumst. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor
+      </p>
+      <div class="linefour"></div>
+      </section>
+      <div class="line-above-codesmith"></div>
+      <h5>CodeSmith</h5>
+      <p>Text</p>
+      <p>Text</p>
+      <p>Text</p>
+      <p>Text</p>
+      <div class="ader">
+      <h6>Header</h6>
+      <p>Text</p>
+      <p>Text</p>
+      <p>Text</p>
+      <p>Text</p>
+      </div>
+      <div class="eader">
+      <h7>Header</h7>
+      <p>Text</p>
+      <p>Text</p>
+      <p>Text</p>
+      <p>Text</p>
+      </div>
+
+
+
+      </main>
+      <footer className="footer">
+        <p>&copy; 2024 CodeSmith | Terms of services | Privacy Notice    CodeSmith 2024. All rights reserved</p>
+      </footer>
+
+
+    </div>
+  );
+}
+export default App;
